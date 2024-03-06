80927f5ac7ec41f9739d796d52f1397a866f14c1
khkim6040 <khkim6040@postech.ac.kr>
2024-03-06 06:45:34 -0800

    Introdue another private variables
    Write some class methods

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -7,20 +7,20 @@
 
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _stream(capacity), _output(capacity), _capacity(capacity), _unassembled_count(0) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _stream(capacity), _output(capacity), _capacity(capacity), _unassembled_count(0), _next_read_point(0), _first_unread_point(-1) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return _unassembled_count;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const {
+    return _unassembled_count == 0;
+}
