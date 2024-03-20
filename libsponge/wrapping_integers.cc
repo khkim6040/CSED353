84e17fa852935fc45306a28209a3dc18eabc2914
khkim6040 <khkim6040@postech.ac.kr>
2024-03-20 03:47:58 -0700

    Pass tests except t_wrapping_ints_roundtrip

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,7 @@
 #include "wrapping_integers.hh"
 
+#include <stdlib.h>
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,7 +16,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    uint64_t divider = 1 << 31;
+    uint64_t divider = 1ul << 32;
     return WrappingInt32(uint32_t((n % divider + isn.raw_value()) % divider));
 }
 
@@ -29,6 +31,27 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    int64_t nn = n.raw_value();
+    int64_t isnn = isn.raw_value();
+
+    // uint64_t diff = n.raw_value() > isn.raw_value() ? n.raw_value() - isn.raw_value() : isn.raw_value() - n.raw_value();
+    uint64_t pre;
+    if (nn - isnn < 0)
+        pre = (1ull << 32) - (isnn - nn) % (1ull << 32);
+    else
+        pre = (nn - isnn) % (1ull << 32);
+
+    uint64_t pre_gap = pre > checkpoint ? pre - checkpoint : checkpoint - pre;
+    for (uint64_t cur = pre + (1ull << 32); cur < UINT64_MAX; cur += (1ull << 32)) {
+        uint64_t cur_gap = cur > checkpoint ? cur - checkpoint : checkpoint - cur;
+        if (cur_gap >= pre_gap) {
+            break;
+        } else {
+            pre = cur;
+            pre_gap = cur_gap;
+        }
+    }
+
+    // cout << n.raw_value() << " " << isn.raw_value() << " " << checkpoint << " " << pre << endl;
+    return pre;
 }
