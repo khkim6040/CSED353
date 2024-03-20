c76d6e8eeae9e2281aa5f16d2a6ed324a2434434
khkim6040 <khkim6040@postech.ac.kr>
2024-03-20 04:26:17 -0700

    Refactor

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -16,8 +16,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    uint64_t divider = 1ul << 32;
-    return WrappingInt32(uint32_t((n % divider + isn.raw_value()) % divider));
+    uint64_t MOD = 1ull << 32;
+    return isn + n % MOD;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -31,19 +31,24 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
+    uint64_t MOD = 1ull << 32;
     int64_t nr = n.raw_value();
     int64_t isnr = isn.raw_value();
-    uint64_t MOD = 1ull << 32;
-    // // uint64_t diff = n.raw_value() > isn.raw_value() ? n.raw_value() - isn.raw_value() : isn.raw_value() - n.raw_value();
+
+    // For modulo operation in C++
     if (nr < isnr)
         nr += MOD;
 
+    // 32-bits
     uint64_t diff = nr - isnr;
 
     if (diff >= checkpoint) {
         return diff;
     }
 
+    //  |diff| .... |diff + k*MOD| ............ |checkpoint| ............. |diff + (k+1)*MOD|
+    //                           |<- left_gap ->|          |<- right_gap ->|
+    // left_gap + right_gap = MOD(=2^32)
     uint32_t left_gap = (checkpoint - diff) % MOD;
     uint32_t right_gap = MOD - left_gap;
     if (left_gap < right_gap) {
@@ -51,17 +56,4 @@ uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     } else {
         return checkpoint + right_gap;
     }
-    // uint64_t pre_gap = pre > checkpoint ? pre - checkpoint : checkpoint - pre;
-    // for (uint64_t cur = pre + (1ull << 32); cur < UINT64_MAX; cur += (1ull << 32)) {
-    //     uint64_t cur_gap = cur > checkpoint ? cur - checkpoint : checkpoint - cur;
-    //     if (cur_gap >= pre_gap) {
-    //         break;
-    //     } else {
-    //         pre = cur;
-    //         pre_gap = cur_gap;
-    //     }
-    // }
-
-    // cout << n.raw_value() << " " << isn.raw_value() << " " << checkpoint << " " << pre << endl;
-    return diff;
 }
